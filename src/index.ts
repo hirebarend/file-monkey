d4f7c692cff48c9b53d74c45992c48fea4eb5b36
Barend Erasmus <hirebarend@gmail.com>
2022-07-21 10:28:04 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,23 @@ import * as uuid from 'uuid';
 
 export function FileMonkey(username: string, password: string) {
   return {
-    onChange: (
-      fn: (collection: {
-        files: Array<{
-          collectionId: string;
-          contentType: string;
-          createdAt: number;
-          id: string;
-          name: string;
-          size: number;
-          tenantId: string;
-          url: string;
-        }>;
-        id: string;
-      }) => void
-    ) => onChange(username, password, fn),
+    onChange: (event: Event) => onChange(username, password, event),
   };
 }
 
-function onChange(
-  username: string,
-  password: string,
-  fn: (collection: {
-    files: Array<{
+async function onChange(username: string, password: string, event: Event) {
+  if (!event.target) {
+    return;
+  }
+
+  const htmlInputElement = event.target as HTMLInputElement;
+
+  if (!htmlInputElement.files) {
+    return;
+  }
+
+  const collection = {
+    files: [] as Array<{
       collectionId: string;
       contentType: string;
       createdAt: number;
@@ -34,43 +28,17 @@ function onChange(
       size: number;
       tenantId: string;
       url: string;
-    }>;
-    id: string;
-  }) => void
-) {
-  return async (event: Event) => {
-    if (!event.target) {
-      return;
-    }
-
-    const htmlInputElement = event.target as HTMLInputElement;
-
-    if (!htmlInputElement.files) {
-      return;
-    }
-
-    const collection = {
-      files: [] as Array<{
-        collectionId: string;
-        contentType: string;
-        createdAt: number;
-        id: string;
-        name: string;
-        size: number;
-        tenantId: string;
-        url: string;
-      }>,
-      id: uuid.v4(),
-    };
-
-    for (const file of htmlInputElement.files) {
-      const result = await post(username, password, collection.id, file);
-
-      collection.files.push(result);
-    }
-
-    fn(collection);
+    }>,
+    id: uuid.v4(),
   };
+
+  for (const file of htmlInputElement.files) {
+    const result = await post(username, password, collection.id, file);
+
+    collection.files.push(result);
+  }
+
+  return collection;
 }
 
 function post(
